7349037768d4e88ea10bd6c755bb18becc393e6d
shotpoet86 <curlybracemaster@gmail.com>
2020-12-04 16:25:03 -0500

    added spinner

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
 import Search from './components/users/Search';
 
+
 import axios from 'axios';
 
 class App extends Component {
